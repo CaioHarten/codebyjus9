cbde8ff6a1d1e1c7cd1b543af09c4bf9c0bd58b3
CaioHarten <102697148+CaioHarten@users.noreply.github.com>
2022-04-17 14:31:19 -0300

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -1,37 +1,38 @@
+var sea,ship;
+var seaImg,shipImg;
 
-function setup() {
+function preload(){
+  seaImg = loadImage("sea.png");
+  shipImg1 = loadAnimation("ship-1.png","ship-1.png",
+                            "ship-2.png","ship-1.png");
+}
+
+function setup(){
   createCanvas(400,400);
-  background(51);
-  box = createSprite(200,200,30,30);
+  background("blue");
 
-}
+  // Plano de fundo móvel
+  sea=createSprite(400,200);
+  sea.addImage(seaImg);
+  sea.velocityX = -5;
+  sea.scale=0.3;
 
-function draw() 
-{
-if (keyIsDown(RIGHT_ARROW))
-{
-  background("red") ;
-}
   
-  if (keyIsDown(LEFT_ARROW)) 
-  {
-    background("blue");
+  ship = createSprite(130,200,30,30);
+  ship.addAnimation("movingShip",shipImg1);
+  ship.scale =0.25;
+  
+}
+
+function draw() {
+  background(0);
+  sea.velocityX = -3;
+
+  
+  //código para redefinir o plano de fundo
+  if(sea.x < 0){
+    sea.x = sea.width/8;
+  }
     
-  }
- 
-    if (keyIsDown(UP_ARROW)) 
-  {
-    background("yellow");
-   
-  }
-
-  if (keyIsDown(DOWN_ARROW)) 
-  {
-    background("green");
-  }
-
-
-  
   drawSprites();
 }
-
